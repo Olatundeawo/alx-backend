11c0c0d314cee3943796c601df25a755f3ff8fc7
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-08-04 14:45:57 -0500

    modifty

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -20,7 +20,7 @@ def get_locale():
 def main() -> str:
     """ The home page
     """
-    return (render_template('2-index.html'))
+    return (render_template('3-index.html'))
 
 
 if __name__ == '__main__':
