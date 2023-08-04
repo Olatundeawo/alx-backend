b488244f2e62da59b4dbb1fff5fec1dfa4285ad6
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-08-04 14:42:39 -0500

    modify a file ;

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,4 +1,27 @@
-from flask_babel import _
+#!/usr/bin/env python3
+"""
+Flask module
+"""
+from flask import request, Flask, render_template
+from flask_babel import Babel
+from config import Config
 
-_('Welcome to Holberton')
-_('Hello world!')
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def main() -> str:
+    """ The home page
+    """
+    return (render_template('2-index.html'))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
