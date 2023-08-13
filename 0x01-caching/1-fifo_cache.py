da723cf035757579317af4c93ffabb0f18fce870
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-08-13 11:13:59 -0500

    modify

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,35 +1,48 @@
-#!/usr/bin/env python3
-"""
-LIFO Caching module
-"""
-from collections import OrderedDict
+#!/usr/bin/python3
+""" FIFO Caching """
+
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """LIFOCache"""
+    """ FIFO caching """
+
     def __init__(self):
+        """ Constructor """
         super().__init__()
-        self.cache_data = {}
+        self.queue = []
 
     def put(self, key, item):
-        """assigning a key to an item
-        """
-        if key is None and item is None:
+        """ Puts item in cache """
+        if key is None or item is None:
             return
-        if key not in self.cache_data:
-            if len(self.cache_data) >= BasicCaching.MAX_ITEMS:
-                remove_first_key = next(iter(OrderedDict(self.cache_data)))
-                # Getting the value of the key removed
-                # key1 = self.cache_data.pop(remove_first_key)
-                print('DISCARD: ', remove_first_key)
+
+        if key not in self.queue:
+            self.queue.append(key)
+        else:
+            self.mv_last_list(key)
+
         self.cache_data[key] = item
 
-    def get(self, key) -> str:
-        """Getting the value of a key
-        """
-        if key is None:
-            return None
-        if self.cache_data.get(key) is None:
-            return None
-        return self.cache_data.get(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = self.get_first_list(self.queue)
+            if first:
+                self.queue.pop(0)
+                del self.cache_data[first]
+                print("DISCARD: {}".format(first))
+
+    def get(self, key):
+        """ Gets item from cache """
+        return self.cache_data.get(key, None)
+
+    def mv_last_list(self, item):
+        """ Moves element to last idx of list """
+        length = len(self.queue)
+        if self.queue[length - 1] != item:
+            self.queue.remove(item)
+            self.queue.append(item)
+
+    @staticmethod
+    def get_first_list(array):
+        """ Get first element of list or None """
+        return array[0] if array else None
